23589516dbe96538f05a5be6bca117e3e127bbf8
MOON <moonjs1018@naver.com>
2024-07-16 10:19:23 +0900

    chore: config 생성

---
+++ frontend/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: "AIzaSyCzClOOwF7g2_b5A_dJwiopGZ-vEDD82bs",
+  authDomain: "teamintro-7b55d.firebaseapp.com",
+  projectId: "teamintro-7b55d",
+  storageBucket: "teamintro-7b55d.appspot.com",
+  messagingSenderId: "1062644024003",
+  appId: "1:1062644024003:web:6c9a0ba6382416840bd876",
+  measurementId: "G-ZTR5BTDZP8"
+};
